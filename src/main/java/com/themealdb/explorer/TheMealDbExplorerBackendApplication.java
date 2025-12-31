db73e5bfc640d8a8bd80eb265c007ab57432fd44
SonaliRborade <boradesonali19@gmail.com>
2025-12-31 21:37:30 +0530

    Initial commit: Spring Boot backend setup

---
+++ src/main/java/com/themealdb/explorer/TheMealDbExplorerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.themealdb.explorer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheMealDbExplorerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheMealDbExplorerBackendApplication.class, args);
+	}
+
+}
